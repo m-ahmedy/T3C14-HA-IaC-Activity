1ba9892dfae2772706c5fee557d62bbcd646a5e6
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2022-12-21 20:01:41 +0200

    Update scripts

--- create.sh
+++ create.sh
@@ -2,4 +2,5 @@
 aws cloudformation create-stack \
     --stack-name $1 \
     --template-body file://$2 \
-    --capabilities CAPABILITY_IAM
+    --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+    --disable-rollback
--- update.sh
+++ update.sh
@@ -2,4 +2,5 @@
 aws cloudformation update-stack \
     --stack-name $1 \
     --template-body file://$2 \
-    --capabilities CAPABILITY_IAM
+    --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+    --disable-rollback
